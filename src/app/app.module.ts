88119a1bb2b9df8d382bfdbd47ea181845942733
Scott Kornblatt <sekorn@scottkosmacbook.home>
2017-03-31 10:45:47 -0400

    new goal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent
   ],
   imports: [
     BrowserModule,
